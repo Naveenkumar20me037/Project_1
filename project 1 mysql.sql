1fe6fd70ce1572c1deff38412c061af8b3151084
Naveenkumar V <138494162+Naveenkumar20me037@users.noreply.github.com>
2023-07-03 20:33:29 +0530

    Add files via upload

---
+++ project 1 mysql.sql
@@ -0,0 +1,23 @@
+create database vnk;
+create table vnk.sp(
+sl_No int,
+name varchar(20),
+Roll_No int,
+Department varchar(20),
+College_Name varchar(20),
+Place varchar(20));
+
+insert into vnk.sp values
+(1,"naveenkumar.V",01,"B.E-Mechanical","MEC","Rasipuram"),
+(2,"kumar",02,"B.E-Mechanical","MEC","Rasipuram"),
+(3,"naveen",03,"B.E-Mechanical","MEC","Rasipuram"),
+(4,"lokesh",04,"B.E-Mechanical","MEC","Rasipuram"),
+(5,"Nelson",05,"B.E-Mechanical","MEC","Rasipuram"),
+(6,"abi",06,"B.E-Mechanical","MEC","Rasipuram"),
+(7,"siva",07,"B.E-Mechanical","MEC","Rasipuram"),
+(8,"kavin",08,"B.E-Mechanical","MEC","Rasipuram"),
+(9,"nithish",09,"B.E-Mechanical","MEC","Rasipuram"),
+(10,"arun",010,"B.E-Mechanical","MEC","Rasipuram");
+
+select * from vnk.sp;
+drop table vnk.sp;
